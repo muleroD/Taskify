574f6b9d053ddb1795f59c537a7971e0ed5f5e86
Richard Mulero <mulerolp@gmail.com>
2023-06-27 02:22:02 -0300

    feat: criando consulta de usuário por id

--- src/main/java/br/com/mulero/taskify/resolver/UserResolver.java
+++ src/main/java/br/com/mulero/taskify/resolver/UserResolver.java
@@ -1,4 +1,22 @@
 package br.com.mulero.taskify.resolver;
 
+import br.com.mulero.taskify.domain.model.User;
+import br.com.mulero.taskify.service.UserService;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class UserResolver {
+
+    private final UserService userService;
+
+    public UserResolver(UserService userService) {
+        this.userService = userService;
+    }
+
+    @QueryMapping
+    public User getUser(@Argument Long id) {
+        return userService.getUserById(id);
+    }
 }
--- src/main/java/br/com/mulero/taskify/service/UserService.java
+++ src/main/java/br/com/mulero/taskify/service/UserService.java
@@ -1,4 +1,19 @@
 package br.com.mulero.taskify.service;
 
+import br.com.mulero.taskify.domain.model.User;
+import br.com.mulero.taskify.domain.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElseThrow();
+    }
 }
