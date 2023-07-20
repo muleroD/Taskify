2f3dd85c49e1591ade26483e6197ae88eb214966
Richard Mulero <mulerolp@gmail.com>
2023-07-20 00:53:24 -0300

    chore: removendo spring security

--- src/main/java/br/com/mulero/taskify/TaskifyApplication.java
+++ src/main/java/br/com/mulero/taskify/TaskifyApplication.java
@@ -2,9 +2,9 @@ package br.com.mulero.taskify;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
 public class TaskifyApplication {
 
     public static void main(String[] args) {
