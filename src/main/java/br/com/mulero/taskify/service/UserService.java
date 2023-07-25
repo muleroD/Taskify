201ef54e4b8cbe57e87ad6ac44df65771070b826
Richard Mulero <mulerolp@gmail.com>
2023-07-24 23:16:25 -0300

    chore: migrando service class > record

--- src/main/java/br/com/mulero/taskify/service/UserService.java
+++ src/main/java/br/com/mulero/taskify/service/UserService.java
@@ -5,13 +5,7 @@ import br.com.mulero.taskify.domain.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService {
-
-    private final UserRepository userRepository;
-
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+public record UserService(UserRepository userRepository) {
 
     public User getUserById(Long id) {
         return userRepository.findById(id).orElseThrow();
