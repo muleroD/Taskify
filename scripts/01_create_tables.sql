54c6eabf7786d24af452f8a0e0cad8d446b4c947
Richard Mulero <mulerolp@gmail.com>
2023-07-24 23:10:01 -0300

    fix: adicionando default schema

--- scripts/01_create_tables.sql
+++ scripts/01_create_tables.sql
@@ -1,8 +1,10 @@
+CREATE SCHEMA IF NOT EXISTS "public";
+
 CREATE TABLE "user"
 (
-    id          SERIAL PRIMARY KEY,
-    name        VARCHAR(255)        NOT NULL,
-    email       VARCHAR(255) UNIQUE NOT NULL,
-    password    CHAR(60)            NOT NULL,
+    id        SERIAL PRIMARY KEY,
+    name      VARCHAR(255)        NOT NULL,
+    email     VARCHAR(255) UNIQUE NOT NULL,
+    password  CHAR(60)            NOT NULL,
     create_at TIMESTAMP           NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
