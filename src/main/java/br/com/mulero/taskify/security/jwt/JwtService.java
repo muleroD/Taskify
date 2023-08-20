eb4f33e36e8b14cc0fddb45ff845d7f1011c5c9f
Richard Mulero <mulerolp@gmail.com>
2023-08-20 02:46:01 -0300

    Enhance JWT management methods and use GrantedAuthority
    Updated JwtService to improve JWT token management. Reworked createToken method to use GrantedAuthority instead of String for role definition. Renamed verifyToken method to decodeToken due to more explicit method name stating its actual purpose. Also added isTokenValid and getUsername methods to improve token handling by checking the token's validity and extracting the username respectively. These changes aim to improve code readability and clarity in token handling procedures.

--- src/main/java/br/com/mulero/taskify/security/jwt/JwtService.java
+++ src/main/java/br/com/mulero/taskify/security/jwt/JwtService.java
@@ -3,6 +3,7 @@ package br.com.mulero.taskify.security.jwt;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import org.springframework.security.core.GrantedAuthority;
 
 import java.util.Date;
 import java.util.List;
@@ -15,7 +16,7 @@ public class JwtService {
     public static final String USERNAME = "username";
     public static final String ROLES = "roles";
 
-    public String createToken(String username, List<String> roles) {
+    public String createToken(String username, List<GrantedAuthority> roles) {
         return JWT.create()
                 .withIssuer(SecurityConstants.ISSUER)
                 .withIssuedAt(new Date())
@@ -26,11 +27,17 @@ public class JwtService {
                 .sign(ALGORITHM);
     }
 
-    public DecodedJWT verifyToken(String token) {
-        JWT.require(ALGORITHM)
+    public DecodedJWT decodeToken(String token) {
+        return JWT.require(ALGORITHM)
                 .build()
                 .verify(token);
+    }
 
-        return JWT.decode(token);
+    public boolean isTokenValid(String token) {
+        return !decodeToken(token).getExpiresAt().before(new Date());
+    }
+
+    public String getUsername(String jwtToken) {
+        return decodeToken(jwtToken).getClaim(USERNAME).asString();
     }
 }
