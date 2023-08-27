8e51618e3f9b137aa2e346f5da30a5787e36d9e6
Richard Mulero <mulerolp@gmail.com>
2023-08-27 03:03:30 -0300

    "Implement improved user registration functionality in UserService
    The registerUser function in UserService has been significantly updated to improve the registration flow for new users. This includes better error handling, such as throwing specific exceptions when a user already exists or a role is not found. Information is now also retrieved from a new source, the RegisterRequest object, instead of the previous UserInput object. Furthermore, upon successful registration, a UserDTO object is now returned rather than the raw User entity."

--- src/main/java/br/com/mulero/taskify/service/UserService.java
+++ src/main/java/br/com/mulero/taskify/service/UserService.java
@@ -1,18 +1,27 @@
 package br.com.mulero.taskify.service;
 
 import br.com.mulero.taskify.domain.model.User;
+import br.com.mulero.taskify.domain.repository.RoleRepository;
 import br.com.mulero.taskify.domain.repository.UserRepository;
+import br.com.mulero.taskify.infrastructure.exception.NotFoundException;
+import br.com.mulero.taskify.infrastructure.exception.UserAlreadyExistException;
 import br.com.mulero.taskify.graphql.projection.UserFilter;
-import br.com.mulero.taskify.graphql.types.UserInput;
-import graphql.GraphqlErrorException;
+import br.com.mulero.taskify.rest.dto.UserDTO;
+import br.com.mulero.taskify.rest.request.RegisterRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Set;
 
 @Service
 @Slf4j
-public record UserService(UserRepository userRepository) {
+public record UserService(UserRepository userRepository, RoleRepository roleRepository) {
+
+    public static final String USER_NOT_FOUND = "Usuário não encontrado com o id: {}";
+    public static final String USER_ALREADY_EXISTS = "Usuário já cadastrado";
+    public static final String ROLE_NOT_FOUND = "Perfil não encontrado";
 
     public List<User> getUsers() {
         return userRepository.findAll();
@@ -20,7 +29,7 @@ public record UserService(UserRepository userRepository) {
 
     public User getUserById(Long id) {
         return userRepository.findById(id).orElseGet(() -> {
-            log.info("Usuário não encontrado com o id: {}", id);
+            log.info(USER_NOT_FOUND, id);
             return null;
         });
     }
@@ -29,17 +38,25 @@ public record UserService(UserRepository userRepository) {
         return userRepository.findAllByFilter(filter);
     }
 
-    public User addUser(UserInput userInput) {
+    public ResponseEntity<UserDTO> registerUser(RegisterRequest newUser) {
         User user = new User();
-        user.setEmail(userInput.email());
+        user.setEmail(newUser.email());
 
         if (userRepository.exists(user.toExample()))
-            throw new GraphqlErrorException.Builder().message("Usuário já cadastrado").build();
+            throw new UserAlreadyExistException(USER_ALREADY_EXISTS);
 
-        user.setName(userInput.name());
-        user.setPassword(userInput.password());
+        user.setName(newUser.name());
+        user.setPassword(newUser.password());
 
-        return userRepository.save(user);
+        roleRepository.findByName(newUser.role()).ifPresentOrElse(
+                role -> user.setRoles(Set.of(role)),
+                () -> {
+                    throw new NotFoundException(ROLE_NOT_FOUND);
+                });
+
+        UserDTO dto = userRepository.save(user).toDTO();
+
+        return ResponseEntity.ok(dto);
     }
 
     public User deleteUser(Long id) {
