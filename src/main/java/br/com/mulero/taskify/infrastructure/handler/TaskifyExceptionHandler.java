945fbe131fe8bb3b54cca00f21c2abebf6c52de8
Richard Mulero <mulerolp@gmail.com>
2023-08-27 18:02:29 -0300

    feat: criando handler para exception generica e ajustando application

--- src/main/java/br/com/mulero/taskify/infrastructure/handler/TaskifyExceptionHandler.java
+++ src/main/java/br/com/mulero/taskify/infrastructure/handler/TaskifyExceptionHandler.java
@@ -4,6 +4,7 @@ import br.com.mulero.taskify.infrastructure.exception.NotFoundException;
 import br.com.mulero.taskify.infrastructure.exception.UserAlreadyExistException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.InvalidDataAccessResourceUsageException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.ErrorResponse;
@@ -30,8 +31,18 @@ public class TaskifyExceptionHandler extends ResponseEntityExceptionHandler {
         return ResponseEntity.badRequest().body(buildApiError(e, HttpStatus.BAD_REQUEST));
     }
 
+    @ExceptionHandler(InvalidDataAccessResourceUsageException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidDataAccessResourceUsageException(InvalidDataAccessResourceUsageException e) {
+        return ResponseEntity.badRequest().body(buildApiError(e, HttpStatus.BAD_REQUEST));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception e) {
+        log.error(e.getMessage(), e);
+        return ResponseEntity.badRequest().body(buildApiError(e, HttpStatus.INTERNAL_SERVER_ERROR));
+    }
+
     private ErrorResponse buildApiError(Exception ex, HttpStatus status) {
-        log.error(ex.getMessage(), ex);
         return ErrorResponse.create(ex, status, ex.getMessage());
     }
 }
