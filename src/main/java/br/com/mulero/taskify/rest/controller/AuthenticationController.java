122230424f34028a10270433e459daddf8300d5e
Richard Mulero <mulerolp@gmail.com>
2023-09-03 00:22:25 -0300

    chore: removendo endpoint de login não utilizado
    Como atualmente a autenticação é feita e controlada pelo AuthenticationFilter.java, não houve a necessidade da criação de um endpoint. As informações do usuário estão sendo enviadas dentro do JWT

--- src/main/java/br/com/mulero/taskify/rest/controller/AuthenticationController.java
+++ src/main/java/br/com/mulero/taskify/rest/controller/AuthenticationController.java
@@ -1,7 +1,6 @@
 package br.com.mulero.taskify.rest.controller;
 
 import br.com.mulero.taskify.rest.dto.UserDTO;
-import br.com.mulero.taskify.rest.request.LoginRequest;
 import br.com.mulero.taskify.rest.request.RegisterRequest;
 import br.com.mulero.taskify.service.UserService;
 import org.springframework.http.ResponseEntity;
@@ -9,16 +8,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import static br.com.mulero.taskify.security.jwt.SecurityConstants.LOGIN_URL;
 import static br.com.mulero.taskify.security.jwt.SecurityConstants.REGISTER_URL;
 
 @RestController
 public record AuthenticationController(UserService userService) {
-
-    @PostMapping(LOGIN_URL)
-    public void authenticate(@RequestBody LoginRequest loginRequest) {
-        System.out.println(loginRequest);
-    }
 
     @PostMapping(REGISTER_URL)
     public ResponseEntity<UserDTO> register(@RequestBody RegisterRequest registerRequest) {
